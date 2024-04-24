c743a2838410e50368116fb94c23a485ad3e3103
JonathanEarle <jonathan.john.enock.earle@gmail.com>
2024-04-24 15:54:22 -0400

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/palm-ui/"
+  base:"/palm-ui/",
   plugins: [tsconfigPaths({ root: __dirname }), react()],
   build: {
     rollupOptions: {
