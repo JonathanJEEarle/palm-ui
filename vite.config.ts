b60caa132a1ae9dd75aedee83b16aa0734166d5a
JonathanEarle <jonathan.john.enock.earle@gmail.com>
2024-04-25 04:09:15 -0400

    ghp

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/palm-ui/",
   plugins: [tsconfigPaths({ root: __dirname }), react()],
+  base: "/palm-ui/",
   build: {
     rollupOptions: {
       output: {
